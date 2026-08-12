fe78b5daac83db8eedb8db95ec2195c6b1129668
gouthamvutukuri <58443137+gouthamvutukuri@users.noreply.github.com>
2026-08-11 20:19:34 -0400

    Add: python-length-function - submission-0

---
+++ Python For Beginners/python-length-function/submission-0.py
@@ -0,0 +1,10 @@
+def get_longer_word(word1: str, word2: str) -> str:
+    return max(word1, word2,key=len)
+    pass
+
+
+
+# do not modify below this line
+print(get_longer_word("yellow", "orange"))
+print(get_longer_word("red", "blue"))
+print(get_longer_word("green", "blue"))
